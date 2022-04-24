a5cf73b4e24124306ba58bf2c87cc29e01ef4efb
Narkomaani <unconfigured@null.spigotmc.org>
2022-04-24 17:05:41 +0300

    Added User.java

---
+++ app/src/main/java/com/example/ht/User.java
@@ -0,0 +1,38 @@
+package com.example.ht;
+
+public class User extends UserProfile {
+    private String username = "mattimeikalainen";
+    private int userid = 12345;
+    private String password = "";
+
+    public User(String username, String password, int userid) {
+        setUsername(username);
+        setPassword(password);
+        setUserid(userid);
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+}
